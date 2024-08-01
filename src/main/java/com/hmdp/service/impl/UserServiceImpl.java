d291272851be341ace7925c0d408e7c7d62260f3
郭雨 <1262425287@qq.com>
2024-08-01 16:34:07 +0800

    由于发送邮件验证码以及登录的功能代码直接在UserController中实现，所以IUserService接口对应的实现类的代码也作出相应调整，删除不必要的接口方法。

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,30 +1,14 @@
 package com.hmdp.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.bean.copier.CopyOptions;
-import cn.hutool.core.lang.UUID;
-import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.hmdp.dto.LoginFormDTO;
-import com.hmdp.dto.Result;
-import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
-import com.hmdp.utils.RegexUtils;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import static com.hmdp.utils.RedisConstants.*;
-import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -42,67 +26,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
-    public Result sendCode(String phone, HttpSession session) {
-        // 1.校验手机号(直接使用封装好的工具类)
-        if (RegexUtils.isPhoneInvalid(phone)) {
-            // 2.如果不符合，返回错误信息
-            return Result.fail("手机号格式不正确");
-        }
-        // 3.符合，生成验证码
-        String code = RandomUtil.randomNumbers(6);
-        // 4.保存验证码到redis，给key加一个业务前缀以示区分，并设置过期时间
-        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
-        // 5.发送验证码
-        log.debug("发送短信验证码成功，验证码: {}", code);
-        // 6.返回成功信息
-        return Result.ok();
+    public User findByPhone(String phone) {
+        User user = query().eq("phone", phone).one();
+        return user;
     }
 
     @Override
-    public Result login(LoginFormDTO loginForm, HttpSession session) {
-        // 1.校验手机号
-        String phone = loginForm.getPhone();
-        if (RegexUtils.isPhoneInvalid(phone)) {
-            // 2.如果不符合，返回错误信息
-            return Result.fail("手机号格式不正确");
-        }
-        // 3.从redis获取验证码，并校验验证码
-        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
-        // 这里的验证码是从之前保存在redis中获取的，需要先从redis中获取，然后与用户输入的验证码进行比较
-        if (cacheCode == null || !cacheCode.equals(loginForm.getCode())) {
-            // 4.验证码错误，返回错误信息
-            return Result.fail("验证码错误");
-        }
-        // 5.验证码正确，查询用户信息（使用MyBatisPlus的查询方法等价于：select * from tb_user where phone = ?）
-        User user = query().eq("phone", phone).one();
-        if (user == null) {
-            // 6.用户不存在，创建新用户并保存
-            user = createUserWithPhone(phone);
-        }
-        // 7. 保存用户信息到redis中
-        // 7.1 随机生成token,作为登录令牌
-        String token = UUID.randomUUID().toString(true);
-        // 7.2 将User对象转为HashMap存储
-        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
-        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(), CopyOptions.create().setIgnoreNullValue(true).setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
-        // 7.3 存储
-        // 使用putAll方法将HashMap中的数据批量保存到redis中
-        String tokenKey = LOGIN_USER_KEY + token;
-        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
-        // 7.4 设置token有效期
-        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
-
-        // 返回token给客户端
-        return Result.ok(token);
-    }
-
-    private User createUserWithPhone(String phone) {
-        // 1.创建用户
+    public User creatUser(String phone) {
         User user = new User();
         user.setPhone(phone);
-        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
-        // 2.保存用户
+        user.setNickName("用户" + phone); // 昵称也设置为电话
+        // 保存用户
         save(user);
         return user;
     }
+
+
 }
